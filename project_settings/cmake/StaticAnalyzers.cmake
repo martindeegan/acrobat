1a794974caeb8d1f312bde6ce568b4c075180f1f
Martin Deegan <mdeegan@umich.edu>
2020-02-29 01:56:25 -0500

    Got clang-tidy and cppchecks in the build

--- project_settings/cmake/StaticAnalyzers.cmake
+++ project_settings/cmake/StaticAnalyzers.cmake
@@ -1,5 +1,5 @@
-option(ENABLE_CPPCHECK "Enable static analysis with cppcheck" OFF)
-option(ENABLE_CLANG_TIDY "Enable static analysis with clang-tidy" OFF)
+option(ENABLE_CPPCHECK "Enable static analysis with cppcheck" ON)
+option(ENABLE_CLANG_TIDY "Enable static analysis with clang-tidy" ON)
 if(ENABLE_CPPCHECK)
   find_program(CPPCHECK cppcheck)
   if(CPPCHECK)
