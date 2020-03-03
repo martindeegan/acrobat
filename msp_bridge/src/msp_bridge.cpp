f19aff8a49895d63a6c9cad9303d329ba3f2cd66
Martin Deegan <mdeegan@umich.edu>
2020-03-02 19:06:37 -0500

    Converted msp_bridge into a component and added a publisher for motors and a controls subscriber

--- msp_bridge/src/msp_bridge.cpp
+++ msp_bridge/src/msp_bridge.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <functional>
 
 #include <msp/Client.hpp>
 #include <msp/msp_msg.hpp>
@@ -6,8 +7,11 @@
 #include <sensor_msgs/msg/imu.hpp>
 
 #include <acrobat_common/composition/visibility_control.hpp>
+#include <acrobat_msgs/msg/acrobat_control.hpp>
+#include <acrobat_msgs/msg/betaflight_motor.hpp>
 
 using namespace std::chrono_literals;
+using std::placeholders::_1;
 
 namespace acrobat::msp_bridge {
 
@@ -15,15 +19,27 @@ class MspBridge : public rclcpp::Node {
   public:
     ACROBAT_COMPOSITION_PUBLIC
 
-    MspBridge() : Node("msp_bridge") {
-        std::string device   = "/dev/ttyUSB0";
-        size_t      baudrate = 115200;
-        msp_client_.start(device, baudrate);
+    MspBridge(const rclcpp::NodeOptions& options) : Node("msp_bridge", options) {
+        declare_parameter("device");
+        declare_parameter("baudrate");
+        declare_parameter("imu_frequency");
+        declare_parameter("motor_frequency");
 
-        imu_pub_ = create_publisher<sensor_msgs::msg::Imu>("/acrobat/fc_imu", 10);
+        auto parameter_client = std::make_shared<rclcpp::SyncParametersClient>(this);
 
-        msp_client_.subscribe(&MspBridge::on_motor, this, 0.01);
-        msp_client_.subscribe(&MspBridge::on_imu, this, 0.005);
+        device_   = parameter_client->get_parameter<std::string>("device");
+        baudrate_ = parameter_client->get_parameter<size_t>("baudrate");
+        msp_client_.start(device_, baudrate_);
+
+        imu_pub_     = create_publisher<sensor_msgs::msg::Imu>("/acrobat/fc_imu", 2);
+        motor_pub_   = create_publisher<acrobat_msgs::msg::BetaflightMotor>("/acrobat/motors", 2);
+        control_sub_ = create_subscription<acrobat_msgs::msg::AcrobatControl>(
+            "/acrobat/control", 2, std::bind(&MspBridge::on_control, this, _1));
+
+        auto imu_frequency   = parameter_client->get_parameter<double>("imu_frequency");
+        auto motor_frequency = parameter_client->get_parameter<double>("motor_frequency");
+        msp_client_.subscribe(&MspBridge::on_motor, this, imu_frequency);
+        msp_client_.subscribe(&MspBridge::on_imu, this, motor_frequency);
 
         imu_msg_.header.frame_id = "/acrobat/fc";
     }
@@ -50,23 +66,31 @@ class MspBridge : public rclcpp::Node {
     }
 
     void on_motor(const msp::msg::Motor& motor) {
+        motor_msg_.stamp = rclcpp::Time();
+
+        motor_msg_.thrust[0] = static_cast<float>(motor.motor[0]);
+        motor_msg_.thrust[1] = static_cast<float>(motor.motor[1]);
+        motor_msg_.thrust[2] = static_cast<float>(motor.motor[2]);
+        motor_msg_.thrust[3] = static_cast<float>(motor.motor[3]);
+
+        motor_pub_->publish(motor_msg_);
+    }
+
+    void on_control(const acrobat_msgs::msg::AcrobatControl::SharedPtr msg) {
         // Do nothing for now
     }
 
-    sensor_msgs::msg::Imu                               imu_msg_;
-    msp::client::Client                                 msp_client_;
-    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub_;
+    size_t                                                             baudrate_;
+    std::string                                                        device_;
+    sensor_msgs::msg::Imu                                              imu_msg_;
+    acrobat_msgs::msg::BetaflightMotor                                 motor_msg_;
+    msp::client::Client                                                msp_client_;
+    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr                imu_pub_;
+    rclcpp::Publisher<acrobat_msgs::msg::BetaflightMotor>::SharedPtr   motor_pub_;
+    rclcpp::Subscription<acrobat_msgs::msg::AcrobatControl>::SharedPtr control_sub_;
 };
 
 } // namespace acrobat::msp_bridge
 
-int main(int argc, char** argv) {
-    rclcpp::init(argc, argv);
-
-    acrobat::msp_bridge::MspBridge bridge;
-
-    rclcpp::WallRate loop_rate(100ms);
-    while (rclcpp::ok()) {
-        loop_rate.sleep();
-    }
-}
+#include "rclcpp_components/register_node_macro.hpp"
+RCLCPP_COMPONENTS_REGISTER_NODE(acrobat::msp_bridge::MspBridge)
